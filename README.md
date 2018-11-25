afbf0e28ac879cc9c74ac550d0a5490c795ea69b
Charles Fraley <cjfraley@uchicago.edu>
2018-11-24 23:24:59 -0800

    Successfully pull random meal from basic meals

--- README.md
+++ README.md
@@ -5,10 +5,15 @@ Make a meal plan. Create a grocery list from it. Monitor nutrition and budget
 ##API resources
 ###API Search
 ##Possible APIs
-https://spoonacular.com
+
+https://www.programmableweb.com/api/kraft-recipe
 edamam (has nutrition analysis and recipe search)
 https://rapidapi.com/webknox/api/Recipe/pricing
-
+https://www.programmableweb.com/api/webknox-recipe
+https://www.programmableweb.com/api/bigoven-recipe
+###Current Focus
+###Spoonacular
+https://spoonacular.com
 ###Food2Fork
 This one may just be down
 https://www.food2fork.com/about/api
--- groceryasst/api_man.py
+++ groceryasst/api_man.py
@@ -1,2 +1,46 @@
+import yaml
+import random
+import pkg_resources
+
+def refresh_cache():
+    stream=pkg_resources.resource_stream("groceryasst", "default_conf/basic_meals.yaml")
+    try:
+        meal_lib=yaml.load(stream)
+        # if type(custom_meals)=="dict":
+    except yaml.YAMLError as exc:
+        print("A problem was found in the basic meals file")
+        raise(exc)
+    with open("meal_lib/cache.yaml", "r") as stream:
+        try:
+            cache_meals=yaml.load(stream)
+            if type(cache_meals)=="dict":
+                meal_lib={**meal_lib, **custom_meals}
+        except yaml.YAMLError as exc:
+            print("A problem was found in your meal cache file")
+            raise(exc)
+    with open("meal_lib/custom.yaml", "r") as stream:
+        try:
+            custom_meals=yaml.load(stream)
+            if type(custom_meals)=="dict":
+                meal_lib={**meal_lib, **custom_meals}
+        except yaml.YAMLError as exc:
+            print("A problem was found in custom meals file")
+            raise(exc)
+    return meal_lib
+
 def findMeal(meal, meal_plan):
-    return "gruel"
+    meal_lib=refresh_cache()
+    yaml.dump(meal_lib)
+    doesnt_fit=True
+    while doesnt_fit:
+        list_of_meals=list(meal_lib.keys())
+        meal_name=random.choice(list_of_meals)
+        meal_from_lib=meal_lib[meal_name]
+        if meal_from_lib["type"]==meal:
+            print("Found matching meal: "+meal_name)
+            return meal_from_lib
+        else:
+            continue
+    # mealName=random
+    # mealName=meal_lib.keys[randint(len(meal_lib.keys()))]
+    # return "gruel"
