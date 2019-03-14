c909d35ccad0325027817be813468a8ae13dcd0c
Charles Fraley <21692054+cjfraley@users.noreply.github.com>
2019-03-13 20:40:32 -0500

    Started search implementation

--- groceryasst/api_man.py
+++ groceryasst/api_man.py
@@ -1,7 +1,26 @@
 import yaml
 import random
 import pkg_resources
+import requests
 
+actual_random = random.SystemRandom()
+
+try:
+    with open("conf/plan.yaml", "r") as stream:
+        try:
+            plan=yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print("A problem was found in your plan config file")
+            raise(exc)
+except:
+    print("No plan file found. Going with defaults.")
+    # with open("conf/default_plan.yaml", "r") as stream:
+    stream=pkg_resources.resource_stream("groceryasst", "default_conf/plan.yaml")
+    try:
+        plan=yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print("A problem was found in your plan config file")
+        raise(exc)
 def refresh_cache():
     stream=pkg_resources.resource_stream("groceryasst", "default_conf/basic_meals.yaml")
     try:
@@ -28,19 +47,40 @@ def refresh_cache():
             raise(exc)
     return meal_lib
 
-def findMeal(meal, meal_plan):
+def findMeal(type_of_meal, meal_plan):
     meal_lib=refresh_cache()
     yaml.dump(meal_lib)
     doesnt_fit=True
     while doesnt_fit:
         list_of_meals=list(meal_lib.keys())
-        meal_name=random.choice(list_of_meals)
+        meal_name=actual_random.choice(list_of_meals)
         meal_from_lib=meal_lib[meal_name]
-        if meal_from_lib["type"]==meal:
+        if meal_from_lib["type"]==type_of_meal:
             print("Found matching meal: "+meal_name)
-            return meal_from_lib
+            return meal_name
         else:
             continue
     # mealName=random
     # mealName=meal_lib.keys[randint(len(meal_lib.keys()))]
     # return "gruel"
+
+def findMealWeb(type_of_meal):
+    payload={}
+    if type_of_meal in ["dessert","breakfast"]:
+        payload["type"]=type_of_meal
+    if "dislikes" in plan.keys():
+        payload["excludeIngredients"]=plan["dislikes"]
+    if "diet" in plan.keys():
+        payload["diet"]=plan["diet"]
+    return search(payload)
+
+def search(payload):
+    # Have to figure out using these types
+    # The type of the recipes. One of the following: main course, side dish, dessert, appetizer, salad, bread, breakfast, soup, beverage, sauce, or drink.
+    response = requests.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex",
+        params=payload,
+        headers={
+            "X-RapidAPI-Key": "K4PobKJKIOmshR7r7VuGzz8cleYtp10dlJojsnOSGbIyPY0rWe"
+        }
+    )
+    return response
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys
 import groceryasst
 
-
 plan = groceryasst.top_level.weeklyPlan()
 
-plan.mealList()
+# plan.mealList()
+
+
+response = groceryasst.api_man.findMealWeb("breakfast")
+
+
+print(response.text)
